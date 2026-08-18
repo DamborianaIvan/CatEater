1db3d174eb76a06464efebbd49d67f07dbb88e39
Ivan G. Damboriana <44871499+DamborianaIvan@users.noreply.github.com>
2026-08-18 17:32:56 -0300

    fix: sincronizar stepsPerFeed con motor

--- firmware/IoTCater/include/services/ConfigurationSyncService.h
+++ firmware/IoTCater/include/services/ConfigurationSyncService.h
@@ -3,6 +3,7 @@
 #include <Arduino.h>
 
 #include "domain/Configuration.h"
+#include "hardware/Motor.h"
 #include "network/ApiClient.h"
 #include "storage/ConfigurationRevisionStorage.h"
 #include "storage/ConfigurationStorage.h"
@@ -12,7 +13,7 @@ class ConfigurationSyncService
    public:
     ConfigurationSyncService(ApiClient& apiClient, ConfigurationStorage& configurationStorage,
                              ConfigurationRevisionStorage& revisionStorage,
-                             Configuration& configuration);
+                             Configuration& configuration, Motor& motor);
 
     void begin();
     void update(uint32_t remoteRevision);
@@ -22,6 +23,7 @@ class ConfigurationSyncService
     ConfigurationStorage& _configurationStorage;
     ConfigurationRevisionStorage& _revisionStorage;
     Configuration& _configuration;
+    Motor& _motor;
 
     uint32_t _localRevision = 0;
 
