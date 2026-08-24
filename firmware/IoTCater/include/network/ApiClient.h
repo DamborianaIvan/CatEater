6cd81a8a21d1fe5d73fa7e343d088a7d31a40560
Ivan G. Damboriana <44871499+DamborianaIvan@users.noreply.github.com>
2026-08-24 14:55:53 -0300

    refactor: simplificar autenticacion permanente del dispositivo

--- firmware/IoTCater/include/network/ApiClient.h
+++ firmware/IoTCater/include/network/ApiClient.h
@@ -9,40 +9,15 @@
 #include "domain/Configuration.h"
 #include "services/BackendConnectionService.h"
 #include "storage/DeviceCredentialStorage.h"
-#include "storage/BootstrapCredentialStorage.h"
-
-enum class RegistrationResult
-{
-    Registered,
-    AlreadyRegistered,
-    Unauthorized,
-    InvalidData,
-    ServerError,
-    ConnectionError
-};
-
-enum class EnrollmentResult
-{
-    Enrolled,
-    AlreadyEnrolled,
-    Unauthorized,
-    NotFound,
-    ServerError,
-    ConnectionError
-};
 
 class ApiClient
 {
    public:
     ApiClient(HttpClient& httpClient, const DeviceInfo& deviceInfo,
               BackendConnectionService& backendConnectionService,
-              DeviceCredentialStorage& deviceCredentialStorage,
-              BootstrapCredentialStorage& bootstrapCredentialStorage);
+              DeviceCredentialStorage& deviceCredentialStorage);
 
-    RegistrationResult registerDevice();
-    EnrollmentResult enrollDevice();
     bool hasDeviceCredential() const;
-    bool hasBootstrapCredential() const;
     bool getFeederInfo(FeederInfo& feederInfo);
     bool getRemoteConfiguration(Configuration& configuration, uint32_t& revision);
     bool getMotorState(bool& motorState, int& portions, String& commandId,
@@ -56,21 +31,14 @@ class ApiClient
     const DeviceInfo& _deviceInfo;
     BackendConnectionService& _backendConnectionService;
     DeviceCredentialStorage& _deviceCredentialStorage;
-    BootstrapCredentialStorage& _bootstrapCredentialStorage;
 
     static constexpr char CONTENT_TYPE[] = "application/json";
     static constexpr char BASE_URL[] = "http://192.168.1.39:5000";
-
-    static constexpr char REGISTER_ENDPOINT[] = "/feeders/register";
-    static constexpr char ENROLL_ENDPOINT[] = "/feeders/enroll";
-    static constexpr char DEVICE_HEARTBEAT_ENDPOINT[] = "/feeders/heartbeat";
     static constexpr uint16_t MOTOR_STATE_TIMEOUT_MS = 500;
     static constexpr uint16_t EVENT_SYNC_TIMEOUT_MS = 1000;
     static constexpr uint16_t BACKGROUND_TIMEOUT_MS = 1000;
 
     String buildUrl(const String& endpoint) const;
-    String buildRegistrationBody() const;
-
     bool canAttemptRequest() const;
     void updateBackendAvailability(const HttpResponse& response);
 };
