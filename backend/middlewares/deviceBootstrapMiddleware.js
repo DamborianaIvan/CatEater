fac3e6fa758e750a363aa69921880b2fe8b44d01
Ivan G. Damboriana <44871499+DamborianaIvan@users.noreply.github.com>
2026-08-25 13:48:44 -0300

    chore: eliminar middleware legacy de bootstrap

--- backend/middlewares/deviceBootstrapMiddleware.js
+++
@@ -1,7 +0,0 @@
-const authenticateDeviceBootstrap = async (req, res) => {
-  return res.status(410).json({
-    error: "El provisioning por bootstrap fue eliminado. El dispositivo debe ser creado y provisionado en fábrica."
-  });
-};
-
-module.exports = authenticateDeviceBootstrap;
