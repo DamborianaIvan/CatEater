6ea52874cf4fa0ff93d1eec9c8c19c6bebcacf20
ivangastondamb@gmail.com <ivangastondamb@gmail.com>
2026-09-04 20:20:43 -0300

    refactor: main refactor

--- firmware/IoTCaterFactory/src/main.cpp
+++ firmware/IoTCaterFactory/src/main.cpp
@@ -4,80 +4,71 @@
 
 namespace
 {
-constexpr uint32_t SERIAL_BAUD_RATE = 115200;
-constexpr size_t EEPROM_SIZE = 512;
+    constexpr uint32_t SERIAL_BAUD_RATE = 115200;
+    constexpr size_t EEPROM_SIZE = 512;
 
-bool clearEeprom()
-{
-    if (!EEPROM.begin(EEPROM_SIZE))
+    bool clearEeprom()
     {
-        return false;
+        EEPROM.begin(EEPROM_SIZE);
+
+        for (size_t address = 0; address < EEPROM_SIZE; ++address)
+        {
+            EEPROM.write(address, 0xFF);
+        }
+
+        const bool committed = EEPROM.commit();
+        EEPROM.end();
+
+        return committed;
     }
 
-    for (size_t address = 0; address < EEPROM_SIZE; ++address)
+    bool formatLittleFs()
     {
-        EEPROM.write(address, 0xFF);
+        return LittleFS.format();
     }
 
-    const bool committed = EEPROM.commit();
-    EEPROM.end();
-
-    return committed;
-}
-
-bool formatLittleFs()
-{
-    if (!LittleFS.begin())
+    void printDeviceIdentity()
     {
-        return false;
+        char deviceId[16];
+        snprintf(deviceId, sizeof(deviceId), "ESP-%06X", ESP.getChipId());
+
+        Serial.println();
+        Serial.println("Device");
+        Serial.print("  Feeder ID       : ");
+        Serial.println(deviceId);
     }
 
-    const bool formatted = LittleFS.format();
-    LittleFS.end();
-
-    return formatted;
-}
-
-void printDeviceIdentity()
-{
-    char deviceId[16];
-    snprintf(deviceId, sizeof(deviceId), "ESP-%06X", ESP.getChipId());
-
-    Serial.println();
-    Serial.println("Device");
-    Serial.print("  Feeder ID       : ");
-    Serial.println(deviceId);
-}
-
-void runFactoryReset()
-{
-    Serial.println();
-    Serial.println("EEPROM");
-    const bool eepromOk = clearEeprom();
-    Serial.print("  Reset           : ");
-    Serial.println(eepromOk ? "OK" : "ERROR");
-
-    Serial.println();
-    Serial.println("LittleFS");
-    const bool littleFsOk = formatLittleFs();
-    Serial.print("  Format          : ");
-    Serial.println(littleFsOk ? "OK" : "ERROR");
-
-    printDeviceIdentity();
-
-    Serial.println();
-    Serial.println("==================================================");
-    if (eepromOk && littleFsOk)
+    void runFactoryReset()
     {
-        Serial.println("  DEVICE READY FOR PROVISIONING");
+        Serial.println();
+        Serial.println("EEPROM");
+        const bool eepromOk = clearEeprom();
+        Serial.print("  Reset           : ");
+        Serial.println(eepromOk ? "OK" : "ERROR");
+
+        Serial.println();
+        Serial.println("LittleFS");
+        const bool littleFsOk = formatLittleFs();
+        Serial.print("  Format          : ");
+        Serial.println(littleFsOk ? "OK" : "ERROR");
+
+        printDeviceIdentity();
+
+        Serial.println();
+        Serial.println("==================================================");
+
+        if (eepromOk && littleFsOk)
+        {
+            Serial.println("  DEVICE READY FOR PROVISIONING");
+        }
+        else
+        {
+            Serial.println("  FACTORY INITIALIZATION FAILED");
+        }
+
+        Serial.println("==================================================");
     }
-    else
-    {
-        Serial.println("  FACTORY INITIALIZATION FAILED");
-    }
-    Serial.println("==================================================");
-}
-}  // namespace
+} // namespace
 
 void setup()
 {
